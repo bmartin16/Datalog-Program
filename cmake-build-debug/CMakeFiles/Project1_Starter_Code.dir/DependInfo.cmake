4c5faf33335d121865e0f78dde8a23ac1f201c22
Blake Martin <18bmartin@gmail.com>
2021-09-25 12:41:47 -0600

    First Commit Project 2

---
+++ cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/18bma/cs236/Datalog-Program/BlockCommentAutomaton.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/BlockCommentAutomaton.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/DatalogProgram.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/DatalogProgram.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/IdAutomaton.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/IdAutomaton.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/IncompleteStringAutomaton.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/IncompleteStringAutomaton.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/InvalidBlockCommentAutomaton.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/InvalidBlockCommentAutomaton.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/Lexer.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Lexer.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/LineCommentAutomaton.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/LineCommentAutomaton.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/MatcherAutomaton.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/MatcherAutomaton.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/Parameter.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Parameter.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/Parser.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Parser.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/Predicate.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Predicate.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/Rule.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Rule.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/StringAutomaton.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/StringAutomaton.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/Token.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/Token.cpp.obj"
+  "C:/Users/18bma/cs236/Datalog-Program/main.cpp" "C:/Users/18bma/cs236/Datalog-Program/cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Project1_Starter_Code.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project1_Starter_Code.dir/BlockCommentAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/DatalogProgram.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/IdAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/IncompleteStringAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/InvalidBlockCommentAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Lexer.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/LineCommentAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/MatcherAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Parameter.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Parser.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Predicate.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Rule.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/StringAutomaton.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/Token.cpp.obj"
+  "CMakeFiles/Project1_Starter_Code.dir/main.cpp.obj"
+  "Project1_Starter_Code.exe"
+  "Project1_Starter_Code.exe.manifest"
+  "Project1_Starter_Code.pdb"
+  "libProject1_Starter_Code.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project1_Starter_Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
